ed30e86cc5748d97ae36743e3c6e336eb353a957
Savio Issa <savioissadesousa@gmail.com>
2024-12-05 09:16:16 -0300

    Finalizando metodos

--- Arvore/src/Arvore.java
+++ Arvore/src/Arvore.java
@@ -23,14 +23,13 @@ public class Arvore {
     }
 
     private Cliente pesquisa(Cliente reg, No no) {
-        if (no == null){
+        if (no == null) {
             return null;
-        }else if (reg.getCpf() < no.reg.getCpf()){
+        } else if (reg.getCpf() < no.reg.getCpf()) {
             return pesquisa(reg, no.esq);
-        }else if (reg.getCpf() > no.reg.getCpf()){
+        } else if (reg.getCpf() > no.reg.getCpf()) {
             return pesquisa(reg, no.dir);
-        }
-        else{
+        } else {
             return no.reg;
         }
     }
@@ -132,5 +131,98 @@ public class Arvore {
         }
     }
 
-}
+    public int numNo() {
+        return numNo(this.raiz);
+    }
 
+    private int numNo(No atual) {
+        if (atual == null) {
+            return 0;
+        }
+        int contagem = 1;
+        contagem += numNo(atual.esq);
+        contagem += numNo(atual.dir); //
+        return contagem;
+    }
+
+    public boolean isomorfica(Arvore av1, Arvore av2) {
+        return isomorfica(av1.raiz, av2.raiz);
+    }
+
+    private boolean isomorfica(No no1, No no2) {
+        if (no1 == null && no2 == null) {
+            return true;
+        }
+        if (no1 == null || no2 == null) {
+            return false;
+        }
+        return no1.reg.equals(no2.reg) && isomorfica(no1.esq, no2.esq) &&
+                isomorfica(no1.dir, no2.dir);
+    }
+
+    public int numFolhas() {
+        return numFolhas(this.raiz);
+    }
+
+    private int numFolhas(No atual) {
+        if (atual == null) {
+            return 0;
+        }
+        if (atual.esq == null && atual.dir == null) {
+            return 1;
+        }
+        return numFolhas(atual.esq) + numFolhas(atual.dir);
+    }
+
+    public boolean balanceada() {
+        return balanceada(this.raiz);
+    }
+
+    private boolean balanceada(No no) {
+        if (no == null) {
+            return true;
+        }
+        int alturaEsq = altura(no.esq);
+        int alturaDir = altura(no.dir);
+        if (Math.abs(alturaEsq - alturaDir) <= 1 && balanceada(no.esq) && balanceada(no.dir)) {
+            return true;
+        }
+        return false;
+    }
+
+    private int altura(No no) {
+        if (no == null) {
+            return 0;
+        }
+        return 1 + Math.max(altura(no.esq),
+                altura(no.dir));
+    }
+
+    public String pathTo(int dado) {
+        StringBuilder caminho = new StringBuilder();
+        if (!pathTo(this.raiz, dado, caminho)) {
+            return "Dado não encontrado na árvore";
+        }
+        return caminho.toString();
+    }
+
+    private boolean pathTo(No no, int dado, StringBuilder caminho) {
+        if (no == null) {
+            return false;
+        }
+        if (no.reg.getCpf() == dado) {
+            return true;
+        }
+        caminho.append("-> Esquerda ");
+        if (pathTo(no.esq, dado, caminho)) {
+            return true;
+        }
+        caminho.delete(caminho.length() - 11, caminho.length());
+        caminho.append("-> Direita ");
+        if (pathTo(no.dir, dado, caminho)) {
+            return true;
+        }
+        caminho.delete(caminho.length() - 10, caminho.length());
+        return false;
+    }
+}
